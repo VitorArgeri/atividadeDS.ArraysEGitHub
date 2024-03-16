88662f8f1cca879ddf5ff1f5a03406fecdfa5411
VitorArgeri <vitorargeri.tds2.senai@gmail.com>
2024-03-16 11:02:42 -0300

    Método 02: .join().

--- Atividade.js
+++ Atividade.js
@@ -1,34 +1,27 @@
-//Método 01: .concat().
-//O método .concat() mistura dois ou mais arrays.
+//Método 02: .join()
+//O método .join() pega todos os elementos de um array e separa eles por um parâmetro definido.
 
 //Const: Usado para criar variáveis que não serão reatríbuidas, ou seja, uma variável imutável.
 
-//Defino um array com as seguintes strings: 'a', 'b', 'c'.
-const arrayA = ['a', 'b', 'c'];
+//Defino uma array com os números 10, 20 e 30.
+const arrayA = [10, 20, 30];
+//Uso do método .join() para separar os elementos da arrayA com o parâmetro *.
+const arrayB = arrayA.join("*");
 //Exibo a arrayA.
 console.log(arrayA);
-//Defino um array com os seguintes números: 1, 2, 3.
-const arrayB = [1, 2, 3];
 //Exibo a arrayB.
 console.log(arrayB);
-//Defino uma array como a mistura entre a arrayA e a arrayB com o .concat().
-const arrayC = arrayA.concat(arrayB);
-//Exibo a arrayC.
-console.log(arrayC);
 
 //Exibo um espaço em branco para separar os dois exemplos.
-console.log("");
+console.log("")
 
-//Defino um array com as seguintes strings:'Pulmão', 'Coração', 'Rim'.
-const arrayD = ['Pulmão', 'Coração', 'Rim'];
+//Defino uma array com as strings 'Aves', 'Mamíferos' e 'Répteis.
+const arrayC = ['Aves', 'Mamíferos', 'Répteis'];
+//Uso do método .join() para separar os elementos da arrayC com o parâmetro _.
+const arrayD = arrayC.join("_");
+//Exibo a arrayC.
+console.log(arrayC);
 //Exibo a arrayD.
 console.log(arrayD);
-//Defino um array com as seguintes strings: 'Pulmão', 'Coração', 'Rim'.
-const arrayE = ['Estômago', 'Instestino'];
-//Exibo a arrayE.
-console.log(arrayE);
-//Defino uma array como a mistura entre a arrayD e a arrayE com o .concat().
-const arrayF = arrayD.concat(arrayE);
-//Exibo a arrayF.
-console.log(arrayF);
+
 
