b1c812335310f8bb82595bc0099763ddfb10974e
VitorArgeri <vitorargeri.tds2.senai@gmail.com>
2024-03-16 13:10:02 -0300

    Método 05: Includes().

--- Atividade.js
+++ Atividade.js
@@ -1,25 +1,19 @@
-//Método 04: .sort().
-//O método.sort() ordena os elementos presentos dentro da array.
+//Método 05: includes().
+//O método includes() verifica se um elemento está presente ou não na array.
 
 //Const: Usado para criar variáveis que não serão reatríbuidas, ou seja, uma variável imutável.
 
-//Defino uma array com os números 5, 4, 3, 2 e 1 em ordem decrescente.
-const numeros = [5, 4, 3, 2, 1];
-//Exibo numeros.
-console.log(numeros);
-//Defino numeros2 como a ordem crescente da array numeros utilizando .sort().
-const numeros2 = numeros.sort();
-//Exibo numeros2.
-console.log(numeros2);
+//Defino uma array com os valores 10, 20 e 30.
+const array1 = [10, 20, 30];
+//Verifico se a array possui o elemento "30" utilizando do .includes(30).
+console.log(array1.includes(30));
+//A resposta será "true".
 
-//Exibo um espaço em branco para separar os dois exemplos.
+//Exibo um espaço em branco para separar os exemplos.
 console.log("");
 
-//Defino uma array com os valores 50, 40, 30, 20 e 10.
-const numeros3 = [50, 40, 30, 20, 10];
-//Exibo a array numeros3
-console.log(numeros3);
-//Defino a a array numeros4 como a forma ordenada da array numeros3 com numeros.sort().
-const numeros4 = numeros3.sort();
-//Exibo numeros4
-console.log(numeros4)
+//Defino uma array com os valores 100, 200 e 300.
+const array2 = [100, 200, 300];
+//Verifico se a array possui o elemento "500" utilizando do .includes(500).
+console.log(array2.includes(500));
+//A resposta será "false".
