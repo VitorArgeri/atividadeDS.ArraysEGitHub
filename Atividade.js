0add97eab35024d72c0d431b0b227fdc1a5cf3c0
VitorArgeri <vitorargeri.tds2.senai@gmail.com>
2024-03-16 14:09:27 -0300

    Método 09: filter().

--- Atividade.js
+++ Atividade.js
@@ -1,21 +1,19 @@
-//Método 08: .every().
-//Verifica se todos os elementos presentes em uma array passam por uma determinada condição.
+//Método 09: filter().
+//Cria uma nova array com todos os elementos que passaram por um determinado teste.
 
-//Defino uma array com os valores 18, 20, 25, 30, 55.
-let idade = [18, 20, 25, 30, 55];
-//Defino a variável verificação como a vericação de que se todos os elementos presentes na array são maiores ou iguais à 18.
-let verificação = idade.every(idade => idade >= 18);
-//Exibo a variável verificação.
-console.log(verificação);
-//A resposta será true.
+//Define a array com os valores 1, 2, 3, 4 e 5.
+let numeros = [1, 2, 3, 4, 5];
+//Define exibidor como a verificação de quais números atendem a condição.
+let exibidor = numeros.filter(numeros => numeros >= 3);
+//Exibe o exibidor,
+console.log(exibidor);
 
-//Exibo um espaço em branco para separar os exemplos.
+//Exibe um espaço em branco para separar os dois exemplos.
 console.log("");
 
-//Defino uma array com os valores 40, 7, 8, 57, 3.
-let mnidade = [2, 3, 4, 5, 18];
-//Defino uma variável verificação2 omo a verifcação de que todos elementos presentes na array são menores do que 18.
-let verificação2 = mnidade.every(mnidade => mnidade < 18);
-//Exibo a variável verificação2.
-console.log(verificação2);
-//A resposta será false.
+//Define uma array coms os valores 10, 20, 30, 40 e 50.
+let numeros2 = [10, 20, 30, 40, 50];
+//Define exibidor2 como a verificação de quais números atendem a condição.
+let exibidor2 = numeros2.filter(numeros2 => numeros2 <= 11);
+//Exibe o exibidor2.
+console.log(exibidor2);
