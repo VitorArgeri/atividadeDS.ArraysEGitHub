94000eaa5f7e93506cc7e02f3bffde98b9145307
VitorArgeri <vitorargeri.tds2.senai@gmail.com>
2024-03-16 13:01:11 -0300

    Método 04: .sort().

--- Atividade.js
+++ Atividade.js
@@ -1,25 +1,25 @@
-//Método 03: .reverse()
-//O método .reverse() faz o que o próprio nome fala, ele inverte a array.
+//Método 04: .sort().
+//O método.sort() ordena os elementos presentos dentro da array.
 
 //Const: Usado para criar variáveis que não serão reatríbuidas, ou seja, uma variável imutável.
 
-//Defino uma array com as strings 1, 2, 3, 4 e 5.
-const arrayA = [1, 2, 3, 4, 5];
-//Exibo a arrayA.
-console.log(arrayA);
-//Defino uma array como o reverso da arrayA com o .reverse().
-const arrayB = arrayA.reverse();
-//Exibo a arrayB.
-console.log(arrayB);
+//Defino uma array com os números 5, 4, 3, 2 e 1 em ordem decrescente.
+const numeros = [5, 4, 3, 2, 1];
+//Exibo numeros.
+console.log(numeros);
+//Defino numeros2 como a ordem crescente da array numeros utilizando .sort().
+const numeros2 = numeros.sort();
+//Exibo numeros2.
+console.log(numeros2);
 
-//Exibo um espaço em branco para separar os exemplos.
-console.log("")
+//Exibo um espaço em branco para separar os dois exemplos.
+console.log("");
 
-//Defino uma array com as strings Abelha, Baleia, Crocodilo e Dinossauro.
-const arrayC = ["Abelha", "Baleia", "Crocodilo", "Dinossauro"];
-//Exibo a arrayC
-console.log(arrayC);
-//Defino uma array como o reverso da arrayC com o .reverse().
-const arrayD = arrayC.reverse();
-//Exibo a arrayD.
-console.log(arrayD);
+//Defino uma array com os valores 50, 40, 30, 20 e 10.
+const numeros3 = [50, 40, 30, 20, 10];
+//Exibo a array numeros3
+console.log(numeros3);
+//Defino a a array numeros4 como a forma ordenada da array numeros3 com numeros.sort().
+const numeros4 = numeros3.sort();
+//Exibo numeros4
+console.log(numeros4)
